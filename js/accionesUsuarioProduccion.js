6a01faebcb0c225b3e38c4f38d6b1d82e0245ecf
Cristina Alonso <crisaloari@gmail.com>
2019-01-30 17:24:42 +0100

    mostrar bien datos

--- js/accionesUsuarioProduccion.js
+++ js/accionesUsuarioProduccion.js
@@ -304,7 +304,7 @@ var oCapaDatos=document.createElement("div");
     	var oListaCap=document.createElement("ul");
     	for(var j=0; j<oSerie.aTemporadas[i].aCapitulos.length;j++){
 			var oLi=document.createElement("li");
-			oLi.textContent="Capítulo "+oSerie.aTemporadas[i].aCapitulos[j].iNumCapitulo+": "+oSerie.aTemporadas[i].aCapitulos[j].sresumen;
+			oLi.textContent="Capítulo "+oSerie.aTemporadas[i].aCapitulos[j].iNumCapitulo+": "+oSerie.aTemporadas[i].aCapitulos[j].sResumen;
 			oListaCap.appendChild(oLi);
     	}
     	oListaTemp.appendChild(oListaCap);
