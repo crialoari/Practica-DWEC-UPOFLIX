0e4e1224eab91566b3baecf0192f073de2a113e7
Cristina Alonso <crisaloari@gmail.com>
2019-01-31 14:27:42 +0100

    organizacion ficheros

--- js/procesarXML.js
+++ js/procesarXML.js
@@ -1,78 +1,77 @@
-var oUpoflix= new Upoflix();
+function loadXMLDoc(filename){
+        if (window.XMLHttpRequest)
+          {
+          xhttp=new XMLHttpRequest();
+          }
+        else // code for IE5 and IE6
+          {
+          xhttp=new ActiveXObject("Microsoft.XMLHTTP");
+          }
+        xhttp.open("GET",filename,false);
+        
+        xhttp.send();
+        
+        return xhttp.responseXML;
+}
 
-function loadXMLDoc(filename)
-{
-	if (window.XMLHttpRequest)
-	  {
-	  xhttp=new XMLHttpRequest();
-	  }
-	else // code for IE5 and IE6
-	  {
-	  xhttp=new ActiveXObject("Microsoft.XMLHTTP");
-	  }
-	xhttp.open("GET",filename,false);
-	
-	xhttp.send();
-	
-	return xhttp.responseXML;
-} 
-
-var oXML=loadXMLDoc("datos.xml");
-var oSeries=oXMl.getElementsByTagName("serie");
-var oPeliculas=oXML.getElementsByTagName("pelicula");
-var oUsuarios=oXML.getElementsByTagName("usuario");
-var oPersonas=oXML.getElementsByTagName("persona");
-var oTemporadas=oXML.getElementsByTagName("temporada");
-var oCapitulos=oXML.getElementsByTagName("capitulo");
-
-for(var i=0;i<oUsuarios.length;i++){
+function cargarDatosPrueba(){
+  var oXML=loadXMLDoc("datos.xml");
+  var oSeries=oXML.getElementsByTagName("serie");
+  var oPeliculas=oXML.getElementsByTagName("pelicula");
+  var oUsuarios=oXML.getElementsByTagName("usuario");
+  var oPersonas=oXML.getElementsByTagName("persona");
+  var oTemporadas=oXML.getElementsByTagName("temporada");
+  var oCapitulos=oXML.getElementsByTagName("capitulo");
+  
+  for(var i=0;i<oUsuarios.length;i++){
     oUpoflix.altaUsuario(new Usuario(oUsuarios[i].querySelector("user").textContent,
-                        oUsuarios[i].querySelector("nombre").textContent,
-                        oUsuarios[i].querySelector("apellido").textContent,
-                        oUsuarios[i].querySelector("email").textContent,
-                        oUsuarios[i].querySelector("contraseña").textContent,
-                        new Date(oUsuarios[i].querySelector("fechaRegistro").textContent),
-                        oUsuarios[i].querySelector("rol").textContent));
-}
-
-for(var i=0;i<oSeries.length;i++){
+              oUsuarios[i].querySelector("nombre").textContent,
+              oUsuarios[i].querySelector("apellido").textContent,
+              oUsuarios[i].querySelector("email").textContent,
+              oUsuarios[i].querySelector("contraseña").textContent,
+              new Date(oUsuarios[i].querySelector("fechaRegistro").textContent),
+              oUsuarios[i].querySelector("rol").textContent));
+  }
+  
+  for(var i=0;i<oSeries.length;i++){
     oUpoflix.añadirProduccion(new Serie(oSeries[i].querySelector("titulo").textContent,
-                            oSeries[i].querySelector("genero").textContent,
-                            [],
-                            [],
-                            oSeries[i].querySelector("resumen").textContent,
-                            oSeries[i].querySelector("urlImagen").textContent,
-                            new Date(oSeries[i].querySelector("fechaInicio").textContent),
-                            new Date(oSeries[i].querySelector("fechaFin").textContent)));
-}
+                oSeries[i].querySelector("genero").textContent,
+                [],
+                [],
+                oSeries[i].querySelector("resumen").textContent,
+                oSeries[i].querySelector("urlImagen").textContent,
+                new Date(oSeries[i].querySelector("fechaInicio").textContent),
+                new Date(oSeries[i].querySelector("fechaFin").textContent)));
+  }
 
-for(var i=0;i<oPeliculas.length;i++){
+  for(var i=0;i<oPeliculas.length;i++){
     oUpoflix.añadirProduccion(new Pelicula(oPeliculas[i].querySelector("titulo").textContent,
-                            oPeliculas[i].querySelector("genero").textContent,
-                            [],
-                            [],
-                            oPeliculas[i].querySelector("resumen").textContent,
-                            oPeliculas[i].querySelector("urlImagen").textContent,
-                            parseInt(oPeliculas[i].querySelector("añoEstreno").textContent),
-                            parseInt(oPeliculas[i].querySelector("duracion").textContent)));
-}
-
-for(var i=0;i<oPersonas.length;i++){
+                oPeliculas[i].querySelector("genero").textContent,
+                [],
+                [],
+                oPeliculas[i].querySelector("resumen").textContent,
+                oPeliculas[i].querySelector("urlImagen").textContent,
+                parseInt(oPeliculas[i].querySelector("añoEstreno").textContent),
+                parseInt(oPeliculas[i].querySelector("duracion").textContent)));
+  }
+  
+  for(var i=0;i<oPersonas.length;i++){
     oUpoflix.altaPersona(new Persona(oPersonas[i].querySelector("nombre").textContent,
-                        oPersonas[i].querySelector("apellido").textContent));
-}
-
-for(var i=0;i<oTemporadas.length;i++){
+              oPersonas[i].querySelector("apellido").textContent));
+  }
+  
+  for(var i=0;i<oTemporadas.length;i++){
     oUpoflix.añadirTemporada(oTemporadas[i].querySelector("tituloSerie").textContent,
-                            oTemporadas[i].querySelector("numTemporada").textContent,
-                            oTemporadas[i].querySelector("resumen").textContent);
-}
-
-for(var i=0;i<oCapitulos.length;i++){
+                oTemporadas[i].querySelector("numTemporada").textContent,
+                oTemporadas[i].querySelector("resumen").textContent);
+  }
+  
+  for(var i=0;i<oCapitulos.length;i++){
     oUpoflix.añadirCapitulo(oCapitulos[i].querySelector("tituloSerie").textContent,
-                            oCapitulos[i].querySelector("numTemporada").textContent,
-                            oCapitulos[i].querySelector("numCapitulo").textContent,
-                            oCapitulos[i].querySelector("resumen").textContent);
+                oCapitulos[i].querySelector("numTemporada").textContent,
+                oCapitulos[i].querySelector("numCapitulo").textContent,
+                oCapitulos[i].querySelector("resumen").textContent);
+  }
+  
+  oUpoflix.iniciarSesion("admin","c123456C");
 }
-
-
